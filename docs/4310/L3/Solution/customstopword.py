04c96e9860dbf4cde603095a2522b80c06e010b0
l33ts <huydqyb@gmail.com>
2020-03-06 22:02:01 +0100

    Delete customstopword.py

--- docs/4310/L3/Solution/customstopword.py
+++
@@ -1,6 +0,0 @@
-from nltk.corpus import stopwords 
-def getstopwords():
-	stopwords_1 = set(stopwords.words('english'))
-	stopwords_2 = set([word.rstrip() for word in open('stopwords.txt', 'r')])
-	stopwords_english = list(set(stopwords_1 | stopwords_2))
-	return stopwords_english
